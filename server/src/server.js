1bf4ea52291b17e4991adb4b035837f3d0e60692
azmarifdev <azmarif.dev@gmail.com>
2023-06-11 22:32:35 +0600

    add

--- server/src/server.js
+++ server/src/server.js
@@ -1,42 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-
 app.use(morgan('dev'));
 
-app.get('/products', (req, res) => {
-    res.status(200).send({
-        message: 'products are returned',
-    });
-});
+const isLoggedIn = (req, res, next) => {
+    console.log('middleware working');
+    next();
+};
+
 app.get('/test', (req, res) => {
     res.status(200).send({
         message: 'server working is fine and',
     });
 });
-app.put('/test', (req, res) => {
+
+app.get('/api/user', isLoggedIn, (req, res) => {
+    console.log('middleware returned');
     res.status(200).send({
-        message: 'put: server working is fine',
-    });
-});
-app.post('/test', (req, res) => {
-    res.status(200).send({
-        message: 'post: server working is fine',
-    });
-});
-app.delete('/test', (req, res) => {
-    res.status(200).send({
-        message: 'delete: server working is fine',
-    });
-});
-app.patch('/test', (req, res) => {
-    res.status(200).send({
-        message: 'patch: server working is fine',
-    });
-});
-app.get('/', (req, res) => {
-    res.status(200).send({
-        message: 'home page is returned',
+        message: 'user found',
     });
 });
 
