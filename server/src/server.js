3126071cc73a27550e4268e2ac2a91145c5eb06c
azmarifdev <azmarif.dev@gmail.com>
2023-07-09 20:58:34 +0600

    reset

--- server/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const { serverPort } = require('./secret');
-
-app.listen(serverPort, () => {
-    console.log(`running server on port http://localhost:${serverPort}`);
-});
